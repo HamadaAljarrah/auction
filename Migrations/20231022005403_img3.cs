7823378b8829fc68afe9f01ae7dd10a059f0a5ce
MarcusOkodugha <100699584+MarcusOkodugha@users.noreply.github.com>
2023-10-22 03:11:12 +0200

    user bid och img from db

---
+++ Migrations/20231022005403_img3.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DistLab2.Migrations
+{
+    /// <inheritdoc />
+    public partial class img3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
