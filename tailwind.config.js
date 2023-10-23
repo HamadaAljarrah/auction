e90f1d325d1f7a371b466e3fbf7e7c0bfe2addc0
MarcusOkodugha <100699584+MarcusOkodugha@users.noreply.github.com>
2023-10-23 12:55:49 +0200

    home

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,22 @@ module.exports = {
             gridTemplateRows: {
                 '6': 'repeat(6, minmax(0, 1fr))',
             },
-            
+
         },
     },
-    plugins: [],
-}
+    plugins: [
+        function ({ addUtilities }) {
+            const newUtilities = {
+                ".no-scrollbar::-webkit-scrollbar": {
+                    display: "none",
+                },
+                ".no-scrollbar": {
+                    "-ms-overflow-style": "none",
+                    "scrollbar-width": "none",
+                },
+            };
+            addUtilities(newUtilities);
+        },
+    ],
+};
 
